04aa7b873324cad3527560dee4c7ab987fabb4d3
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2019-06-13 17:41:29 +0200

    Populate page with false data

--- src/app/podcast-detail/podcast-detail.component.ts
+++ src/app/podcast-detail/podcast-detail.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PodcastDetailComponent implements OnInit {
 
+  podcast = {
+    title: "This is a podcast.",
+    author: "Me",
+    src: "https://sipios.com/team"
+  };
+
   constructor() { }
 
   ngOnInit() {
