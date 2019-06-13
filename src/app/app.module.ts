a387a3ae9a9aab072c08ad35e940673c16568970
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2019-06-13 16:57:16 +0200

    Add first lists elements.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PodcastsSuggestionsComponent } from './podcasts-suggestions/podcasts-suggestions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PodcastsSuggestionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
