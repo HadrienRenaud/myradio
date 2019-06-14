1e8682958cc4537ae262a32cf3624c41018058a3
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2019-06-14 18:38:56 +0200

    Refacto Download button

--- src/app/podcast-detail/podcast-detail.component.ts
+++ src/app/podcast-detail/podcast-detail.component.ts
@@ -11,7 +11,7 @@ import { environment } from 'src/environments/environment';
 export class PodcastDetailComponent implements OnInit {
 
   podcastId;
-  podcast;
+  podcast = {};
 
   constructor(
     private route: ActivatedRoute,
